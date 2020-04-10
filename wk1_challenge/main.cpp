1bd466e16976ac66aabd9525928a1d20c47d9a5a
EC2 Default User <ec2-user@ip-172-31-91-55.ec2.internal>
2020-04-10 18:46:13 +0000

    complete wk1

---
+++ wk1_challenge/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+
+std::stack<int> reverse_stack(std::stack<int> s) {
+  std::stack<int> reversed_s; 
+  // write code here that returns a stack whose elements are
+  // in reverse order from those in stack s
+  for (int i=0; s.size(); i++){
+    int top = s.top();
+    s.pop();
+    reversed_s.push(top);
+  };
+  
+  return reversed_s;
+}
+
+std::queue<int> reverse_queue(std::queue<int> q) {
+  std::queue<int> reversed_q;
+  std::stack<int> s; 
+
+  // write code here that returns a queue whose elements are
+  // in reverse order from those in queue q
+  for (int i=0; q.size(); i++){
+    int front = q.front();
+    q.pop();
+    s.push(front);
+  };
+  for (int i=0; s.size(); i++){
+    int top = s.top();
+    s.pop();
+    reversed_q.push(top);
+  };
+
+  return reversed_q;
+}
+
+void print_stack(std::string name, std::stack<int> s) {
+  std::cout << "stack " << name << ": ";
+  while (!s.empty()) {
+    std::cout << s.top() << " ";
+    s.pop();
+  }
+  std::cout << std::endl;
+}
+
+void print_queue(std::string name, std::queue<int> q) {
+  std::cout << "queue " << name << ": ";
+  while (!q.empty()) {
+    std::cout << q.front() << " ";
+    q.pop();
+  }
+  std::cout << std::endl;
+}
+
+int main() {
+  std::stack<int> s, rs;
+  std::queue<int> q, rq;
+  
+  s.push(1); s.push(2); s.push(3); s.push(4); s.push(5);
+
+  print_stack("s",s);
+
+  rs = reverse_stack(s);
+
+  print_stack("reversed s",rs);
+  
+  q.push(1); q.push(2); q.push(3); q.push(4); q.push(5);
+  
+  print_queue("q",q);
+  
+  rq = reverse_queue(q);
+  
+  print_queue("reversed q",rq);
+
+  return 0;
+}
